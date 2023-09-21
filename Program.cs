cf63f1020ac8e6a2bb8046dae11bfc29decd1b2e
Martin <martin.maquilan@edu.sait.ca>
2023-09-20 22:48:45 -0600

    Fixed minor output issue

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
-            Person person2 = new Person(2, "Gina", "James", "Red", 18, false);
+            Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
             Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45,  true);
             Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28,  true);
 
@@ -59,7 +59,6 @@
                     Console.WriteLine(person.ToString());
                 }
             }
-
 
             foreach (Person person in people)
             {
